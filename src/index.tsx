59ada9c9596173e7926f4f70d12594c66d0607f8
Lucas Munhoz <ln.munhoz@gmail.com>
2020-09-12 18:25:18 +0700

    fix: css import order

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
 
-import "./index.css"
 import "antd/dist/antd.css"
+import "./index.css"
 
 ReactDOM.render(
    <React.StrictMode>
